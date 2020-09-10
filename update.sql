d29b160f4fcd01293d41fe3b3fcc6329c212f84b
Ali Dhobey <alidhobey@gmail.com>
2020-09-10 11:34:58 -0500

    Rollback data

--- update.sql
+++ update.sql
@@ -38,18 +38,18 @@ WHERE invoice_number = '7628231837';
 
 --An employee was recently fired so we must delete them from our database. Delete the employee with employee_id of 35.
 --What problems might you run into when deleting? How would you recommend fixing it?
-DELETE FROM employees
-WHERE employee_id = 35 AND employee_type_id = 2 and sale_id = 35;
+DELETE FROM sales s
+WHERE s.sale_id = 35;
+
+DELETE FROM employees 
+WHERE employee_id = 35;
 
 SELECT * FROM employees
 WHERE employee_id = 35;
 
-SELECT * FROM employeetypes;
-WHERE employee_id = 35;
-
 SELECT * FROM sales
 WHERE employee_id = 35;
-
+ ROLLBACK;
 
 
 
