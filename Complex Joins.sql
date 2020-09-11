6b563900011aff87b90c93d47e1a638156eb9458
Ali Dhobey <alidhobey@gmail.com>
2020-09-10 19:01:17 -0500

    Updated comlext query

--- Complex Joins.sql
+++ Complex Joins.sql
@@ -2,8 +2,13 @@
 ---Practice: Sales Type by Dealership
 --Produce a report that lists every dealership, the number of purchases done by each, and the number of leases done by each.
 
-SELECT DISTINCT(d.business_name), s.invoice_number, st.name
+SELECT 
+	d.business_name,
+	count(s.sale_id) as number_of_sales 
 FROM dealerships d
-join sales s ON s.sale_id = s.dealership_id 
-JOIN salestypes st ON st.sales_type_id = s.sale_id;
+JOIN sales s ON s.dealership_id = s.dealership_id 
+JOIN salestypes st ON s.sales_type_id = st.sales_type_id
+WHERE st.sales_type_id = 1
+GROUP BY d.dealership_id
+ORDER BY  d.dealership_id;
  
