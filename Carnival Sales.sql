ba376f44e02fdc50344617b691b5314655360f39
Ali Dhobey <alidhobey@gmail.com>
2020-09-15 10:27:32 -0500

    new query add

--- Carnival Sales.sql
+++ Carnival Sales.sql
@@ -8,3 +8,11 @@ SELECT d.business_name, sum(s.price)
 FROM sales s
 JOIN dealerships d ON s.dealership_id = d.dealership_id
 GROUP BY d.dealership_id;
+
+--Write a query that shows the purchase sales income per dealership for the current month.
+
+SELECT d.business_name, sum(s.price)
+FROM sales s
+JOIN dealerships d ON s.dealership_id = d.dealership_id
+WHERE date_part('month', s.purchase_date) = date_part('month', CURRENT_DATE)
+GROUP BY d.dealership_id;
