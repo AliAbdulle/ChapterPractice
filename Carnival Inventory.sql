da2b0abacd502cfed37340f075d29c31967d6ad9
Ali Dhobey <alidhobey@gmail.com>
2020-09-16 18:37:12 -0500

    dealership sales

--- Carnival Inventory.sql
+++ Carnival Inventory.sql
@@ -24,3 +24,24 @@ JOIN vehiclemodels vm ON vt.model_id = vm.vehicle_model_id
 JOIN vehiclemakes mk ON vt.make_id = mk.vehicle_make_id
 GROUP BY vm.vehicle_model_id, mk.name
 ORDER BY COUNT(v.vehicle_type_id)desc;
+
+--Diverse Dealerships
+
+--Which dealerships are currently selling the least number of vehicle models? This will let dealerships market vehicle models more effectively per region.
+
+SELECT
+	d.business_name,
+	COUNT(md.vehicle_model_id) as TOTAl_count
+FROM sales s
+JOIN dealerships d ON s.dealership_id = d.dealership_id
+JOIN vehicles v ON v.vehicle_id = v.vehicle_id
+JOIN vehicletypes vt ON vt.vehicle_type_id = vt.vehicle_type_id
+JOIN vehiclebodytypes bt ON vt.body_type_id = bt.vehicle_body_type_id
+JOIN vehiclemodels md ON vt.model_id = md.vehicle_model_id
+GROUP by d.dealership_id
+ORDER by COUNT(md.vehicle_model_id);
+
+
+
+
+
