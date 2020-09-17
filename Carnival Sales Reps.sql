906a1f02f80629a254b4d2384da8c3ee9ddec9e8
Ali Dhobey <alidhobey@gmail.com>
2020-09-16 19:04:10 -0500

    TOP 3 employees dealership

--- Carnival Sales Reps.sql
+++ Carnival Sales Reps.sql
@@ -19,3 +19,20 @@ JOIN dealershipemployees de ON de.employee_id = e.employee_id
 JOIN dealerships d ON de.dealership_id = d.dealership_id
 WHERE LOWER(et.name) LIKE '%finance%'
 GROUP BY et.employee_type_id;
+
+--3. Get the names of the top 3 employees who work shifts at the most dealerships?
+
+SELECT
+	e.first_name,
+	e.last_name,
+	COUNT(d.dealership_id)
+FROM employees e
+JOIN dealershipemployees de ON de.employee_id = e.employee_id
+JOIN dealerships d ON de.dealership_id = d.dealership_id
+GROUP BY e.employee_id
+ORDER BY COUNT(d.dealership_id) desc
+LIMIT 3;
+
+
+
+
